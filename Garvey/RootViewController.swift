14898787039986fe7efcec817012070367485e91
Wikipedia Brown <wikipediabrown@iamgoodbad.com>
2019-06-11 15:44:31 -0700

    initial commit

---
+++ Garvey/RootViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RootViewController.swift
+//  Garvey
+//
+//  Created by Wikipedia Brown on 6/11/19.
+//  Copyright © 2019 IamGoodBad. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
